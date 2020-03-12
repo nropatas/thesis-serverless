d0e615e66ead3ec2913fe8d4fcae9b95dd49755f
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2020-03-13 00:31:25 +0200

    Add serverless' config and handler for a lambda function

---
+++ functions/aws/handler.js
@@ -0,0 +1,9 @@
+module.exports.simple = async (event) => {
+  return {
+    statusCode: 200,
+    body: 'Hello',
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
