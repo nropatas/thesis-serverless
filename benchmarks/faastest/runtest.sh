620e07222bb6f09c98bea84ba61370980f6ca054
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2020-05-12 00:08:26 +0300

    Add a simple script for running benchmarks iteratively

---
+++ benchmarks/faastest/runtest.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+  echo "Please specify a command"
+  exit 1
+fi
+
+if [[ -z $2 || $2 == 0 ]]
+then
+  echo "Please specify the number of iterations"
+  exit 1
+fi
+
+if [[ -z $3 && $2 > 1 ]]
+then
+  echo "Please specify the time interval between iterations (e.g., 30s, 5m)"
+  exit 1
+fi
+
+cmd=$1
+iter=$2
+interval=$3
+
+for i in `seq 1 $iter`
+do
+  eval $cmd
+
+  if [[ $i < $iter ]]
+  then
+    sleep $interval
+  fi
+done
