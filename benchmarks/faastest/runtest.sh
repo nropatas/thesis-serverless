b8f4cfd2afd2f1cf21818d9d80ba6c1f2be281ff
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2020-05-12 02:44:56 +0300

    Correct runtest script

--- benchmarks/faastest/runtest.sh
+++ benchmarks/faastest/runtest.sh
@@ -1,33 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z $1 ]
+if [ -z "$1" ]
 then
   echo "Please specify a command"
   exit 1
 fi
 
-if [[ -z $2 || $2 == 0 ]]
+if [[ -z "$2" || $(($2)) == 0 ]]
 then
   echo "Please specify the number of iterations"
   exit 1
 fi
 
-if [[ -z $3 && $2 > 1 ]]
+if [[ -z "$3" && $(($2)) > 1 ]]
 then
   echo "Please specify the time interval between iterations (e.g., 30s, 5m)"
   exit 1
 fi
 
 cmd=$1
-iter=$2
+iter=$(($2))
 interval=$3
 
 for i in `seq 1 $iter`
 do
+  echo "$i of $iter"
   eval $cmd
 
-  if [[ $i < $iter ]]
+  if (( i < iter ))
   then
+    echo "Sleeping for $interval..."
     sleep $interval
   fi
 done
