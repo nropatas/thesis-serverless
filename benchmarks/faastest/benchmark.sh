13b6d1abe37a05e754c38ef2ca75224dc3855827
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2020-05-28 14:43:47 +0300

    Add script for automating benchmarks

---
+++ benchmarks/faastest/benchmark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# providers=(azure)
+providers=(aws knative openfaas kubeless fission)
+tests=(BurstLvl1)
+iterations=10
+interval=5m
+
+for provider in ${providers[*]}
+do
+  for test in ${tests[*]}
+  do
+    echo "Testing $provider $test"
+    ./runtest.sh "./faasbenchmark run $provider $test -r results/$test/$provider" $iterations $interval
+    echo "$provider $test done!"
+    echo "Sleeping for $interval"
+    sleep $interval
+  done
+done
