8ad56ccdaa1661a1c0c54d48030819c1178e1cdc
Sataponn Phutrakul <sataponn.phutrakul@aalto.fi>
2020-06-14 18:01:52 +0300

    Revert "Move worker nodes to the same availability zone"
    This reverts commit 2bb6c4afe8bd9b4ce7444a9b735089674982cdbd.

--- clusters/eks/main.tf
+++ clusters/eks/main.tf
@@ -3,12 +3,16 @@ provider "aws" {
   region      = var.region
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
 
   name                 = "eks-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = ["${var.region}a"]
+  azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.1.0/24", "10.0.3.0/24"]
   private_subnets      = ["10.0.2.0/24", "10.0.4.0/24"]
   enable_nat_gateway   = true
